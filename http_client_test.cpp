b08de1afdf4d24a074ee92fccda7d289545706a7
unknown <King Lee@A02101303188.(none)>
2014-09-28 13:40:19 +0800

    add test

--- http_client_test.cpp
+++ http_client_test.cpp
@@ -11,11 +11,11 @@ using namespace  network;
 
 static std::mutex       s_post_mutex;
 #ifndef __CLOUD_BOX
-//#define __CLOUD_BOX
+#define __CLOUD_BOX
 #endif //__CLOUD_BOX
 
 #ifndef __CLOUD_BOX_FORMAL
-#define __CLOUD_BOX_FORMAL
+//#define __CLOUD_BOX_FORMAL
 #endif //__CLOUD_BOX_FORMAL
 
 #ifdef __CLOUD_BOX
@@ -27,6 +27,10 @@ static std::mutex       s_post_mutex;
 #else
 #define HTTP_URL		"http://192.168.22.61:20000/"
 #endif // __CLOUD_BOX
+
+#ifndef __ALI
+#define __ALI
+#endif //__ALI
 
 #ifdef __ALI
 #define HTTP_URL2	"http://115.29.42.238:5000/" 
@@ -91,9 +95,31 @@ public:
 
 	void post7();
 
+	void post8();
+
 	//	test for wanli
 	void test_participle();
 
+	void test_voice_query(const char* __query);
+
+	void test_set_colour();
+
+	void test_modify_info();
+
+	void test_feedback();
+
+	void test_get_user_info();
+
+	void test_contracts();
+
+	void test_compass();
+
+	void test_select_date();
+
+	void test_express_register();
+
+	void test_express_login();
+	
 	//Http Response Callback
 	void onHttpRequestCompleted(network::HttpClient *sender, network::HttpResponse *response);
 
@@ -253,9 +279,9 @@ void HttpClientTest::post4(int __msg_type)
 	json_t* __msg = json_object();
 	json_t* __msg_id = json_integer(__msg_type);
 	json_t* __flow_id = json_integer(88888888);
-	json_t* __activity_type = json_integer(1);
+	json_t* __activity_type = json_integer(3);
 	json_t* __msg_channel = json_string("000023");
-	json_t* __msg_version = json_string("1.2.6");
+	json_t* __msg_version = json_string("1.2.8");
 	json_object_set(__msg, "msg_id", __msg_id);
 	json_object_set(__msg, "flowid", __flow_id);
 	json_object_set(__msg, "activity_type", __activity_type);
@@ -318,8 +344,8 @@ void HttpClientTest::post5()
 	// decref for json object
 	json_decref(__msg_id);
 }
-static std::string __device_guid = "6d1dc7bd3f6e38f49685167f83fe29e2";
-static int __race_time = 7120;
+static std::string __device_guid = "6d1dc7bd3f6e38f49685167f83fe29e2lee2";
+static int __race_time = 99745;
 void HttpClientTest::post6()
 {
 	//	post data to http server
@@ -411,6 +437,45 @@ void HttpClientTest::post7()
 	json_decref(__msg_id);
 }
 
+void HttpClientTest::post8()
+{
+	//	post data to http server
+	std::string __string_token = "&token=1234567788";
+	std::string __string_msg = "msg=";
+	json_t* __msg = json_object();
+	json_t* __msg_id = json_integer(/*TYPE_RANDOM_PRIZE*/13);
+	json_t* __flow_id = json_integer(88888888);
+	json_t* __msg_channel = json_string("000023");
+	json_t* __msg_version = json_string("1.2.8");
+	json_t* __single_gacha = json_string("false");
+	json_t* __msg_device_guid = json_string(__device_guid.c_str());
+	json_object_set(__msg, "msg_id", __msg_id);
+	json_object_set(__msg, "flowid", __flow_id);
+	json_object_set(__msg, "channel", __msg_channel);
+	json_object_set(__msg, "version", __msg_version);
+	json_object_set(__msg, "deviceid", __msg_device_guid);
+	json_object_set(__msg, "single_gacha", __single_gacha);
+	HttpRequest* request = new HttpRequest();
+	request->setUrl(HTTP_URL);
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	const char* postData = json_dumps(__msg,0);
+	if(postData)
+	{
+		__string_msg  += postData;
+		__string_msg += __string_token;
+		request->setRequestData(__string_msg.c_str(), __string_msg.length());
+	}
+	request->setTag("POST test8\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+
+	// decref for json object
+	json_decref(__msg_id);
+}
+
 void HttpClientTest::onHttpRequestCompleted( network::HttpClient *sender, network::HttpResponse *response )
 {
 	if (!response)
@@ -485,24 +550,248 @@ void HttpClientTest::test_participle()
 	request->release();
 }
 
+void HttpClientTest::test_voice_query(const char* __query)
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "voice_query";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1535&version=1&voice_content=";
+	__str_msg += __query;
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_voice_query\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_get_user_info()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "userinfo";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1525&sex=1&v=1&type=0";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_get_user_info\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_set_colour()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "set_colour";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1525&colour=2";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_set_colour\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_feedback()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "feedback";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1525&content=我要留言";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_feedback\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_express_register()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "register";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "user=lee&password=lee&sex=1";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_express_login\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_express_login()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "login";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "user=lee&password=lee";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_express_login\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_modify_info()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "modifyInfo";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1525&name=King Lee&pass=164271579&bd=1983040404&sex=1&ba=9";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_set_colour\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_contracts()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "contacts";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	//std::string __str_msg = "uid=1525&type=get";
+	//std::string __str_msg = "uid=1525&contracts_uid=1123&contracts_name=李希文&type=add";
+	std::string __str_msg = "uid=1525&contracts_uid=1123&type=del";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_contracts\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_compass()
+{
+		//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "compass";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1525&type=1";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_compass\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
+void HttpClientTest::test_select_date()
+{
+	//	post data to http server
+	HttpRequest* request = new HttpRequest();
+	std::string __url = HTTP_URL2;
+	__url += "select_date";
+	request->setUrl(__url.c_str());
+	request->setRequestType(HttpRequest::Type::POST);
+	request->setResponseCallback(this, httpresponse_selector(HttpClientTest::onHttpRequestCompleted));
+
+	// write the post data
+	std::string __str_msg = "uid=1525&select_date_type=8&days_type=1";
+	__str_msg = _UnicodeToUTF8(_AnsiToUnicode(__str_msg));
+	request->setRequestData((const char*)__str_msg.c_str(), __str_msg.length());
+	request->setTag("POST test_select_date\n");
+	HttpClient::getInstance()->send(request);
+	request->release();
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	HttpClientTest* __test = new HttpClientTest();
 	if(1)
 	{
-		if(1)
+		if(0)
 		{
-			__test->test_participle();
+			//__test->test_participle();
+			//__test->test_voice_query("今天运程如何");
+			//__test->test_voice_query("我本月做事如何");
+			//__test->test_voice_query("我今年能量如何");
+			//__test->test_voice_query("我今天去旅行如何");
+			//__test->test_voice_query("我今日健康如何");
+			//__test->test_voice_query("我今日财富如何");
+			//__test->test_voice_query("我今日破财么");
+			//__test->test_voice_query("我本月购物如何");
+			//__test->test_voice_query("我本月学业如何");
+			//__test->test_voice_query("我今年事业如何");
+			//__test->test_voice_query("我今日求财如何");
+			//__test->test_voice_query("我今日情感如何");
+			//__test->test_voice_query("我今日会友如何");
+			//__test->test_voice_query("我今日感情变化如何");
+			//__test->test_voice_query("我今日桃花如何");
+			//__test->test_voice_query("我今日追求好么");
+			__test->test_voice_query("我这天那个方向最顺?");
+			//__test->test_voice_query("问点什么好呢?");
+			//__test->test_get_user_info();
+			//__test->test_set_colour();
+			//__test->test_modify_info();
+			//__test->test_feedback();
+			//__test->test_contracts();
+			//__test->test_compass();
+			__test->test_select_date();
+			//__test->test_express_register();
+			//__test->test_express_login();
 			//__test->post1();
+
+			//	test express
+
 		}
 		else
 		{
-			__test->post2();
+			//__test->post2();
 			//__test->post3();
 			__test->post4(5);
 			//__test->post5();
-			__test->post6();
-			__test->post7();
+			//__test->post6();
+			//__test->post7();
+			//__test->post8();
 		}
 
 	}
